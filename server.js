e156f7c4938c888a5d86de45e6c40ca4bd86605f
Josh Kim <josh94kim@berkeley.edu>
2017-09-13 14:41:54 -0700

    added mongoose

---
+++ server.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// makes Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+const {DATABASE_URL, PORT} = require('./config');
+const {BlogPost} = require('./models');
+
+const app = express();
+app.use(morgan('common'));
+app.use(bodyParser.json);
+
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .then(blogPosts => {
+      res.json({
+        blogPosts: blogPosts.map(
+          (blogPost) => blogPost.apiRepr())
+      });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+    .findById(req.params.id)
+    .then(blogPost => res.json(blogPost.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(blogPost => res.status(201).json(blogPost.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id ${req.params.id} and request body id ` +
+      ` ${req.body.id} must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  const toUpdate = [];
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  BlogPost
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+    .then(blogPost => res.status(200).json(blogPost.apiRepr))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(blogPost => res.status(204).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+let server 
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing the server');
+      server.close(err => {
+        if (err) {
+          // return to ensure that we don't also call resolve
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+// if server.js is called directly (w/ node), runServer is called
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
